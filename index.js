3eccc58d362eb6df4db7004cf4e3e8a9b5de903e
Michael Kuehl <michael@getstructure.io>
2016-04-14 15:35:19 -0400

    update structure refs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var five = require('johnny-five');
 var raspi = require('raspi-io');
-var Device = require('structure-sdk-js').Device;
+var Device = require('losant-sdk-js').Device;
 
-// Construct Structure device.
+// Construct Losant device.
 var device = new Device({
   id: 'my-device-id',
   key: 'my-access-key',
   secret: 'my-access-secret'
 });
 
-// Connect the device to Structure.
+// Connect the device to Losant.
 device.connect();
 
 var board = new five.Board({
@@ -22,11 +22,11 @@ board.on('ready', function() {
   var button = new five.Button('GPIO21');
 
   button.on('down', function() {
-    // When the button is pressed, send the state to Structure.
+    // When the button is pressed, send the state to Losant.
     device.sendState({ button: true });
   });
 
-  // Listen for commands from Structure.
+  // Listen for commands from Losant.
   device.on('command', function(command) {
     if(command.name === 'toggle') {
       led.toggle();
